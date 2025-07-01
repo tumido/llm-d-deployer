88ebb31bbf249af09ccf2de35f7f59612d76641e
Daneyon Hansen <daneyon.hansen@solo.io>
2025-07-01 07:58:35 -0700

    Charts: Bumps Kgateway to v2.0.3 (#353)
    Signed-off-by: Daneyon Hansen <daneyon.hansen@solo.io>

--- chart-dependencies/kgateway/install.sh
+++ chart-dependencies/kgateway/install.sh
@@ -6,13 +6,13 @@ if [[ "$MODE" == "apply" ]]; then
   helm upgrade -i \
     --namespace kgateway-system \
     --create-namespace \
-    --version v2.0.0 \
+    --version v2.0.3 \
     kgateway-crds oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds
 
   helm upgrade -i \
     --namespace kgateway-system \
     --create-namespace \
-    --version v2.0.0 \
+    --version v2.0.3 \
     --set inferenceExtension.enabled=true \
     --set securityContext.allowPrivilegeEscalation=false \
     --set securityContext.capabilities.drop={ALL} \
@@ -22,5 +22,5 @@ if [[ "$MODE" == "apply" ]]; then
 else
   helm uninstall kgateway --ignore-not-found  --namespace kgateway-system || true
   helm uninstall kgateway-crds --ignore-not-found --namespace kgateway-system || true
-  helm template kgateway-crds oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds --version v2.0.0 | kubectl delete -f - --ignore-not-found
+  helm template kgateway-crds oci://cr.kgateway.dev/kgateway-dev/charts/kgateway-crds --version v2.0.3 | kubectl delete -f - --ignore-not-found
 fi
